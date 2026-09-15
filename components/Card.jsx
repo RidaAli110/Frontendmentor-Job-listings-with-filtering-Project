20e7d62e34ae8ab8d4c08357deb381df61ed3394
RidaAli110 <rida.webdev110@gmail.com>
2026-09-15 21:05:17 +0100

    Started working on responsiveness for mobile

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,41 +1,61 @@
+import JobTags from './JobTags';
+
 export default function Card({ job }) {
   return (
     <div
-      className={`flex gap-6 p-6 max-w-6xl w-full
-     bg-gray-50 shadow-[0_2px_6px_rgba(0,0,0,0.08)] rounded-sm
+      className={` flex flex-col md:flex-row  relative  gap-6 p-6 pt-10 md:pt-6
+        max-w-6xl w-full bg-gray-50 shadow-[0_2px_6px_rgba(0,0,0,0.08)] rounded-sm
      ${job.featured ? 'border-l-4 border-l-(--blue-text)' : ''}`}
     >
-      <img className='w-16 h-16' src={job.logo} alt='Company Logo' />
+      <img
+        className='w-16 h-16 absolute -top-8
+        left-6 md:static md:top-auto md:left-auto'
+        src={job.logo}
+        alt='Company Logo'
+      />
       {/* Container for company info and job details */}
-      <section className='flex flex-col '>
+      <section className='flex flex-col  '>
         {/* Company info */}
-        <div className='flex gap-3'>
-          <h1 className='text-(--blue-text) font-bold'>{job.company}</h1>
-          {job.new && <p>NEW!</p>}
-          {job.featured && <p>FEATURED</p>}
+        <div className='flex gap-4'>
+          <h1
+            className='  text-(--blue-text) font-bold text-sm mb-1
+          hover:text-(--blue-text)'
+          >
+            {job.company}
+          </h1>
+          {job.new && (
+            <p
+              className=' flex justify-center items-center text-white 
+              font-bold bg-(--blue-text) text-[12px]
+              rounded-l-full rounded-r-full px-2 py-0.5 '
+            >
+              NEW!
+            </p>
+          )}
+          {job.featured && (
+            <p
+              className=' flex justify-center items-center
+           text-white font-bold bg-black text-[12px]
+           rounded-l-full rounded-r-full px-2 py-0.5'
+            >
+              FEATURED
+            </p>
+          )}
         </div>
         {/* Job details */}
-        <div className='flex flex-col'>
+        <div className='flex flex-col font-bold gap-1'>
           <p>{job.position}</p>
-          <div className='flex flex-row text-(--grey-text)'>
+          <div className='flex flex-row gap-4 text-sm font-light text-(--grey-text)'>
             <p>{job.postedAt}</p>
+            <span>•</span>
             <p>{job.contract}</p>
+            <span>•</span>
             <p>{job.location}</p>
           </div>
         </div>
       </section>
       {/* Job tags */}
-      <div className='flex ml-auto gap-2'>
-        <button>{job.role}</button>
-        <button>{job.level}</button>
-        {job.languages.map((language) => {
-          return <button key={language}>{language}</button>;
-        })}
-
-        {job.tools.map((tool) => {
-          return <button key={tool}>{tool}</button>;
-        })}
-      </div>
+      <JobTags job={job} />
     </div>
   );
 }
