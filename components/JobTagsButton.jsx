74629b746253d69771250f77a9d197aea05d39e7
RidaAli110 <rida.webdev110@gmail.com>
2026-09-17 18:27:33 +0100

    Changed hover text color

--- components/JobTagsButton.jsx
+++ components/JobTagsButton.jsx
@@ -3,7 +3,7 @@ export default function JobTagsButton({ children }) {
     <button
       className='text-(--blue-text) text-sm font-semibold
      bg-(--job-tag-bg) px-2 py-0.5 rounded
-      hover:bg-(--blue-text) hover:text-white cursor-pointer '
+      hover:bg-(--blue-text) hover:text-(--job-tag-bg) cursor-pointer '
     >
       {children}
     </button>
