0609a104dcc1b069e9a4524c05b98f7a690fa34f
RidaAli110 <rida.webdev110@gmail.com>
2026-09-19 22:18:48 +0100

    Created the Filter button

---
+++ components/FilterButton.jsx
@@ -0,0 +1,14 @@
+export default function FilterButton({ children }) {
+  return (
+    <div
+      className='flex items-center  gap-2 text-(--blue-text) text-sm font-semibold
+     bg-(--job-tag-bg) pl-2  rounded cursor-pointer'
+    >
+      <p className="py-0.5">{children}</p>
+      <button className=' self-stretch hover:bg-black bg-(--blue-text) p-1
+      rounded-r '>
+        <img src='/images/icon-remove.svg' alt='remove' />
+      </button>
+    </div>
+  );
+}
