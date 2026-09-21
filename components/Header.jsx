92ec74a2b5b649e02644f95b5ba49be56833cb30
RidaAli110 <rida.webdev110@gmail.com>
2026-09-21 17:02:35 +0100

    Fixed header bg color

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className='bg-[hsl(180,31%,44%)]'>
+    <header className='bg-(--blue-text)'>
       <img
         className='block w-full h-35 object-cover pointer-events-none'
         src='/images/bg-header-desktop.svg'
