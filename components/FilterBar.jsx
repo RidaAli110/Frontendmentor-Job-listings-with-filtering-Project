7e50d676cfa89420164fbb0fe02b02338f338c57
RidaAli110 <rida.webdev110@gmail.com>
2026-09-21 17:00:54 +0100

    Adjusted filter bar size to be the same size as the cards

--- components/FilterBar.jsx
+++ components/FilterBar.jsx
@@ -3,7 +3,7 @@ import FilterButton from './FilterButton';
 export default function FilterBar({ filters, removeFilter, clearFilters }) {
   return (
     <section
-      className=' flex items-center gap-2 w-full p-5 absolute -top-28 left-0
+      className=' flex items-center gap-2 mx-auto inset-x-0 max-w-6xl w-full p-5 absolute -top-28 left-0
      rounded-sm bg-white shadow-[0_2px_6px_rgba(0,0,0,0.08)]'
     >
       <div className='flex flex-wrap gap-2 '>
